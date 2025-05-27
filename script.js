d3e4fb597197ddbe800107415aa955d14c818e9b
ReanuKeeve <taran.in.china@gmail.com>
2025-05-27 22:18:28 +0800

    Many changes

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const video = document.querySelector('video');
-const form = document.getElementById('contactForm');
+const form = document.getElementById('book-form');
 const services = document.querySelectorAll('.service-item');
 const header = document.querySelector('header');
-
-
-document.getElementById('bookNow').addEventListener('click', function() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-});
-
+const bookNow = document.getElementById('bookNow');
+const phoneInputField = document.querySelector("#phone");
+const info = document.querySelector(".alert-info");
+const phoneInput = window.intlTelInput(phoneInputField, {
+ 
+     utilsScript:
+       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+   });
 window.addEventListener('scroll', function() {
     services.forEach(service => {
         const position = service.getBoundingClientRect();
@@ -38,3 +35,21 @@ window.addEventListener('scroll', function() {
         header.classList.remove('sticky');
     }
 });
+
+bookNow.addEventListener('click', () => {
+    form.classList.add('show');
+    form.classList.remove('hidden');
+});
+
+const onInput = event => {
+    event.target.value = event.target.value.replace(/[^0-9+]/g, '')
+};
+
+function process(event) {
+ event.preventDefault();
+
+ const phoneNumber = phoneInput.getNumber();
+
+ info.style.display = "";
+ info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+}
